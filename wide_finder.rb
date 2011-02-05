91a6f3773b3bc99a5972602123810cb17ff555c3
Evan Light <evan@tripledogdare.net>
2011-02-04 21:47:52 -0500

    Slightly faster

--- wide_finder.rb
+++ wide_finder.rb
@@ -1,21 +1,18 @@
 class WideFinder
   def initialize(filename)
-    url_histogram = nil
+    url_histogram = Hash.new(0)
     File.open(filename, "r") do |handle|
       while line = handle.gets
         line =~ /"([A-Z]+)\s([^\s\?]+)/
-        url = $2
-        hash[url] += 1
-        hash
+        url_histogram[$2] += 1
       end
     end
-    unformatted_results = Array(url_histogram).sort! { |a, b| a[1] <=> b[1] }.reverse
-    @results = unformatted_results.inject([]) do |results, result| 
-      results << "#{result[1]}: #{result[0]}"
-    end
+    @results = url_histogram.sort { |a, b| a[1] <=> b[1] }.reverse
   end
 
   def results(how_many)
-    @results[0, how_many]
+    @results[0,how_many].inject([]) do |results, result| 
+      results << "#{result[1]}: #{result[0]}"
+    end
   end
 end
